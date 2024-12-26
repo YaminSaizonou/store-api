5edf0f2042d5d12e9b05bdccc89a47c38e221c66
YaminSaizonou <yaminsaizonou@icloud.com>
2024-12-26 19:58:57 +0100

    rewrote the function so it can overwrite the previous quantity

--- controllers/addSize.js
+++ controllers/addSize.js
@@ -1,7 +1,25 @@
 const handleAddSize = async (req, res, postgres) => {
-    try{
+    try {
         const { item_id, size_id, quantity } = req.body;
-        const itemSize = await postgres
+
+        // Check if the item_size already exists
+        const existingItemSize = await postgres
+            .select('*')
+            .from('item_sizes')
+            .where({ item_id, size_id })
+            .first();
+
+        if (existingItemSize) {
+            // If the item_size exists, update the quantity
+            const updatedItemSize = await postgres('item_sizes')
+                .where({ item_id, size_id })
+                .update({ quantity })
+                .returning('*');
+
+            res.json(updatedItemSize[0]);
+        } else {
+            // If the item_size does not exist, insert a new record
+            const newItemSize = await postgres
                 .insert({
                     item_id: item_id,
                     size_id: size_id,
@@ -9,15 +27,15 @@ const handleAddSize = async (req, res, postgres) => {
                 })
                 .into('item_sizes')
                 .returning('*');
-                
-                res.json(itemSize[0]);
+
+            res.json(newItemSize[0]);
+        }
+    } catch (error) {
+        console.error('Error adding or updating the size:', error);
+        res.status(500).json({ error: 'Error adding or updating the size' });
     }
-    catch (error) {
-        console.error('Error adding the size:', error);
-        res.status(500).json({ error: 'Error adding the size' });
-    }
-}
+};
 
 module.exports = {
     handleAddSize: handleAddSize
-}
+};
